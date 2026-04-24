0c4808f0063a5c44ba7faa9bf64e44eba2c9c110
Hugo <hugomoreiralaprano@gmail.com>
2026-04-23 21:15:53 -0300

    mudança no labirinto e correção de bugs

--- button_comecar.gd
+++ button_comecar.gd
@@ -1,9 +1,27 @@
-func _on_button_pressed():
-	var nome = $VBoxContainer/LineNome.text
-	var ra = $VBoxContainer/LineRA.text
+#func _on_button_pressed():
+	#var nome = $VBoxContainer/LineNome.text
+	#var ra = $VBoxContainer/LineRA.text
+	#
+	#if nome == "" or ra == "":
+		#print("Preencha tudo!")
+		#return
+	#
+	#get_tree().change_scene_to_file("res://Principal.tscn")
+
+extends Button
+# Como o LineNome e o ButtonComecar estão no mesmo nível, 
+# basta usar ".." para referenciar o próprio nível ou usar o nome direto se fossem filhos.
+# Para a sua árvore, o caminho correto é este:
+@onready var line_nome = get_node("../LineNome")
+@onready var line_ra = get_node("../LineRA")
+
+func _on_pressed():
+	var nome = line_nome.text
+	var ra = line_ra.text
 	
 	if nome == "" or ra == "":
 		print("Preencha tudo!")
 		return
 	
-	get_tree().change_scene_to_file("res://Principal.tscn")
+	print("Indo para a cena principal...")
+	get_tree().change_scene_to_file("res://principal.tscn")
